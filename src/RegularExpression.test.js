1b3b72a2384aaa20d8e6aded7cc1b33475816702
Mako Kusuda <mako.kusuda@gmail.com>
2022-07-07 07:45:31 +0900

    Add comment

--- src/RegularExpression.test.js
+++ src/RegularExpression.test.js
@@ -19,12 +19,14 @@ describe("Regular expression", () => {
   test("can input text", async () => {
     await driver.get("https://regex101.com/");
 
+    // Input regular expression
     const textArea = await driver.findElement(
       By.xpath("//*[@aria-label='insert your regular expression here']")
     );
     await driver.actions().move(textArea).perform();
     await driver.actions().sendKeys("0d{1,4}-d{1,4}-d{4}").perform();
 
+    // Check string
     const value = await textArea.getAttribute("value");
     expect(value).toContain("0d{1,4}-d{1,4}-d{4}");
   });
