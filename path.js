873ff4421ffd8ab394bd024b67229154aaf03d8b
Mako Kusuda <mako.kusuda@gmail.com>
2022-07-09 16:19:21 +0900

    Update test for explanation

---
+++ path.js
@@ -0,0 +1,15 @@
+const PATH = {
+  index: "https://regex101.com/",
+
+  // Right side
+
+  // Middle
+  regularExpressionInput:
+    "//*[@aria-label='insert your regular expression here']",
+
+  // Left side
+  // Explanation card
+  equivalentCharacter: "(//*[@class='T0laQ']/span)",
+};
+
+module.exports = PATH;
--- src/explanation.test.js
+++ src/explanation.test.js
@@ -1,6 +1,7 @@
 const { Builder, By, until } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const service = new chrome.ServiceBuilder("./chromedriver");
+const PATH = require("../path");
 
 describe("Explanation card", () => {
   let driver;
@@ -16,30 +17,30 @@ describe("Explanation card", () => {
     driver.quit();
   });
 
-  test("should display description", async () => {
-    await driver.get("https://regex101.com/");
+  test("should display correct description", async () => {
+    await driver.get(PATH.index);
 
     // Input regular expression
     const textArea = await driver.findElement(
-      By.xpath("//*[@aria-label='insert your regular expression here']")
+      By.xpath(PATH.regularExpressionInput)
     );
     await driver.actions().move(textArea).perform();
     await driver.actions().sendKeys(`\\d\\D`).perform();
 
     // Wait until description card displays
     await driver.wait(
-      until.elementLocated(By.xpath("(//*[@class='T0laQ']/span)[1]")),
+      until.elementLocated(By.xpath(`${PATH.equivalentCharacter}[1]`)),
       10000
     );
 
     const digit = await driver
-      .findElement(By.xpath("(//*[@class='T0laQ']/span)[1]"))
+      .findElement(By.xpath(`${PATH.equivalentCharacter}[1]`))
       .getText();
     const character = await driver
-      .findElement(By.xpath("(//*[@class='T0laQ']/span)[2]"))
+      .findElement(By.xpath(`${PATH.equivalentCharacter}[2]`))
       .getText();
 
-    // Regular expression matches a digit and any character
+    // Check if explanation is correct
     expect(digit).toBe("[0-9]");
     expect(character).toBe("[^0-9]");
   });
