eeb5766a62fe1c20c2b92d0cf669e1e28a89da93
Mako Kusuda <mako.kusuda@gmail.com>
2022-07-09 16:24:01 +0900

    Update test for function

--- path.js
+++ path.js
@@ -2,10 +2,14 @@ const PATH = {
   index: "https://regex101.com/",
 
   // Right side
+  // Function
+  substitution: "//*[@class='wcfss']//*[@class='u_Q0A'][text()='Substitution']",
 
   // Middle
   regularExpressionInput:
     "//*[@aria-label='insert your regular expression here']",
+  // Substitution
+  substitutionTitle: "//*[@class='dYInr JOzNE z2wCE llpmv']//*[@class='JOzNE']",
 
   // Left side
   // Explanation card
